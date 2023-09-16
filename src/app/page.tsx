2c7508309bcf0e01bc2fba3c0cbc9d74c669943d
Alex Ma <alexma390@outlook.com>
2023-09-16 10:41:22 -0700

    project setup

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <h1 className="text-3xl">Weave</h1>
+      <Button>Get Started!</Button>
+    </main>
+  );
+}
