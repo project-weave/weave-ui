12ab7232057c24df23c0a090b262c9e45c594f80
kaikw3 <kaikoyamawong@gmail.com>
2023-12-05 02:35:04 -0800

    test: Test Commit on Landing-Page Branch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
       <h1 className="text-3xl">Weave</h1>
-      <Button>Get Started!</Button>
+      <Link href="/(event)">
+      <Button>Event Page!</Button>
+      </Link>
+
+      <Link href="/landng-page.tsx">
+      <Button>landing page!</Button>
+      </Link>
     </main>
   );
 }
