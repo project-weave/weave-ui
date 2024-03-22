e0d87d3a1774e566ea8556614d53d4544075d076
2bato <andrewfan030114@gmail.com>
2024-03-21 20:21:27 -0700

    chore: implement new landing page design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,11 @@
 "use client";
-import SpeechBubble from "@/components/ui/speech-bubble";
-import { ChevronDown } from "lucide-react";
 import { useRef } from "react";
 
 import NewEventPage from "./(event)/new/page";
 
-const TEXT_1 = "Is anyone free for a quick meeting this week?";
-const TEXT_2 = "flights to LA in June are on sale 🛩️ what dates work?";
-const TEXT_3 = "girls night out 👯🥂! are you free this weekend?";
-const TEXT_4 = "let's study over midterm break!";
-const TEXT_5 = "Hey team! What days work best for our weekly basketball practice? 🏀⛹️";
-const TITLE = "find time for what's important";
-const SUBTITLE = "create. add. share.";
+const TITLE = "find time for \n what's important";
+const SUBTITLE =
+  "The hardest part about getting together is finding time for it. With Weave, you'll be able to schedule group events with ease, and enjoy doing so.";
 
 export default function LandingPage() {
   const titleRef = useRef<HTMLDivElement | null>(null);
@@ -19,98 +13,26 @@ export default function LandingPage() {
   return (
     <div className="flex flex-col items-center">
       <div
-        className="mt-10 grid max-h-[70rem] w-full"
+        className="mt-8 grid max-h-[70rem] w-full"
         style={{
           gridTemplateRows: "40% 20% 40%"
         }}
       >
-        <div className="flex h-full w-full items-center justify-between pt-6 text-xl text-white">
-          <div className="mr-20 flex flex-row self-end">
-            <SpeechBubble
-              animationDelay={0.5}
-              avatarColour="#B9AFCC"
-              colour="#B58DFF"
-              height="7.4rem"
-              isAvatarLeft={true}
-              width="16rem"
-            >
-              {TEXT_1}
-            </SpeechBubble>
-          </div>
-          <div className="grid h-full grid-cols-2 grid-rows-2">
-            <div className="mb-4">
-              <SpeechBubble
-                animationDelay={1}
-                avatarColour="#FBC2DA"
-                colour="#4F3F6B"
-                height="6rem"
-                isAvatarLeft={false}
-                width="20rem"
-              >
-                {TEXT_2}
-              </SpeechBubble>
-            </div>
-            <div className="col-start-2 row-start-2 ml-6 mt-4">
-              <SpeechBubble
-                animationDelay={2}
-                avatarColour="#E0B8FF"
-                colour="#907AB7"
-                height="6rem"
-                isAvatarLeft={false}
-                width="18rem"
-              >
-                {TEXT_3}
-              </SpeechBubble>
-            </div>
-          </div>
-        </div>
-        <h1 className="mb-10 mt-5 text-center text-[5rem] font-semibold tracking-wide">send a weave.</h1>
-        <div className="flex h-full w-full items-center justify-between pt-10 text-xl text-white">
-          <div className="mb-20 ml-20">
-            <SpeechBubble
-              animationDelay={1.5}
-              avatarColour="#B2AD96"
-              colour="#907AB7"
-              height="6rem"
-              isAvatarLeft={true}
-              width="16rem"
-            >
-              {TEXT_4}
-            </SpeechBubble>
-          </div>
-
-          <div className="col-start-2 row-start-2 mr-16 mt-8">
-            <SpeechBubble
-              animationDelay={1}
-              avatarColour="#4E4E4E"
-              colour="#4D2E87"
-              height="8rem"
-              isAvatarLeft={true}
-              width="22rem"
-            >
-              {TEXT_5}
-            </SpeechBubble>
-          </div>
-        </div>
+        <h1 className="mt-5 whitespace-pre-line text-center text-[5rem] font-semibold tracking-wide">{TITLE} </h1>
       </div>
 
-      <div className="mb-8 mt-14 cursor-pointer">
-        <ChevronDown
-          className="duration-100 hover:translate-y-2"
-          height={60}
-          onClick={() => {
-            titleRef.current?.scrollIntoView({ behavior: "smooth", block: "start" });
-          }}
-          width={60}
-        />
-      </div>
+      <h1 className="tracing-wider w-4/5 content-center pt-10 text-center text-2xl font-medium">{SUBTITLE}</h1>
 
-      <div className="tracing-wider pt-24 font-semibold" ref={titleRef}>
-        <h1 className="text-3xl">{TITLE}</h1>
-        <h3 className="mt-4 text-center text-lg">{SUBTITLE}</h3>
-      </div>
+      <button
+        className="text-m mt-16 h-auto rounded-2xl bg-primary px-12 py-2 align-bottom font-bold text-primary-foreground outline-none transition-colors hover:bg-primary-hover focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-1 disabled:pointer-events-none disabled:opacity-50"
+        onClick={() => {
+          titleRef.current?.scrollIntoView({ behavior: "smooth", block: "start" });
+        }}
+      >
+        Create Event
+      </button>
 
-      <div className="pb-24 pt-10">
+      <div className="pb-24 pt-24" ref={titleRef}>
         <NewEventPage />
       </div>
     </div>
