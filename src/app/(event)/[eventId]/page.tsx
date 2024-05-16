7bb2e7e967a83609e7555b23cc45684313f71c18
Alex Ma <alexma390@outlook.com>
2024-05-15 20:03:37 -0700

    chore: fix lint error

--- src/app/(event)/[eventId]/page.tsx
+++ src/app/(event)/[eventId]/page.tsx
@@ -71,7 +71,7 @@ export default function Event() {
   const endTime = parseISO(getTimeSlot(event.endTime));
 
   while (currentTime <= endTime) {
-    let formattedTime = format(currentTime, EVENT_TIME_FORMAT);
+    const formattedTime = format(currentTime, EVENT_TIME_FORMAT);
     sortedEventTimes.push(formattedTime);
     currentTime = addMinutes(currentTime, TIME_SLOT_INTERVAL_MINUTES);
   }
