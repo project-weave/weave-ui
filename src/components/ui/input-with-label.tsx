ca044dbac32a03f8bc37a29fce235395b43efd84
2bato <andrewfan030114@gmail.com>
2025-02-24 09:01:21 -0700

    feat: input components style update

--- src/components/ui/input-with-label.tsx
+++ src/components/ui/input-with-label.tsx
@@ -17,7 +17,7 @@ const InputWithLabel = forwardRef<HTMLDivElement, InputWithLabelProps>(({ error,
       <Input
         {...props}
         className={cn(
-          "peer flex h-11 w-full appearance-none border-2 border-primary border-opacity-40 bg-background px-4 pb-2.5 pt-3 hover:border-opacity-100 focus:border-[3px] focus:border-opacity-100 focus:outline-none",
+          "peer flex h-11 w-full appearance-none bg-input px-4 pb-2.5 pt-3 hover:border-opacity-100",
           error && "border-red-500"
         )}
         id={id}
@@ -25,7 +25,7 @@ const InputWithLabel = forwardRef<HTMLDivElement, InputWithLabelProps>(({ error,
         type={type}
       />
       <Label
-        className="absolute left-1 top-1 z-10 origin-[0] -translate-y-3.5 scale-75 transform rounded-sm bg-background px-3 text-[.9rem] duration-300 peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:scale-100 peer-placeholder-shown:text-[.9rem] peer-focus:top-1 peer-focus:-translate-y-3.5 peer-focus:scale-75 peer-focus:px-2"
+        className="text-text-light absolute left-1 top-1 z-10 origin-[0] -translate-y-3.5 scale-75 transform rounded-sm bg-input px-3 text-[.9rem] duration-300 peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:scale-100 peer-placeholder-shown:text-[.9rem] peer-focus:top-1 peer-focus:-translate-y-3.5 peer-focus:scale-75 peer-focus:px-2"
         htmlFor={id}
       >
         {label}
--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -11,7 +11,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(({ className, error
   return (
     <input
       className={cn(
-        "placeholder:text-muted-foreground peer flex w-full rounded-2xl border border-primary bg-background px-3 py-2  file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50",
+        "placeholder:text-muted-foreground peer flex w-full rounded-xl bg-input px-3 py-2  file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50",
         error && "border-red-500",
         className
       )}
